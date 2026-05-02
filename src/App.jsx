c86aa2236d4e21b1c8ee9ba944ab631a50ba4057
NoufalZidan <noufal.ivannov@mhs.unsoed.ac.id>
2026-05-02 16:18:18 +0700

    Add Protected Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Profile from './pages/Profile'
-import Register from './pages/Register'
-import Login from './pages/Login'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/profile/:id' element={<Profile />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile/:id"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import { supabase } from "../lib/supabase";
+
+const ProtectedRoute = ({ children }) => {
+  const [session, setSession] = useState(undefined);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+  }, []);
+
+  if (session === undefined) return null;
+
+  if (!session) return <Navigate to="/login" />;
+
+  return children;
+};
+
+export default ProtectedRoute;
